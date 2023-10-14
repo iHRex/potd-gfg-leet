19ccc8726ea9295523660ba236da367280f39c2d
Harsh Verma <harsverm@gmail.com>
2023-10-14 22:48:48 +0530

    14 October Updated

---
+++ Leetcode/October/14-py-solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def paintWalls(self, cost, time):
+        """
+        :type cost: List[int]
+        :type time: List[int]
+        :rtype: int
+        """
+        dp = [float("inf")]*(len(cost)+1)
+        dp[0] = 0
+        for c, t in itertools.izip(cost, time):
+            for j in reversed(xrange(1, len(cost)+1)):
+                dp[j] = min(dp[j], dp[max(j-(t+1), 0)]+c)
+        return dp[-1]
+        
